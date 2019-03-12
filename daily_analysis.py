921ddf70d31bc86bf934f88df9c5cd987ba17485
Ani <37546222+AniDas10@users.noreply.github.com>
2019-03-12 13:45:01 +0530

    Update daily_analysis.py
    Final touches with comments added

--- daily_analysis.py
+++ daily_analysis.py
@@ -1,22 +1,9 @@
 import matplotlib.pyplot as plt
 import csv
 import pandas as pd
-#data_set = {
-#    'SAP':[11,10,28,49,105],
-#    'NAME':["Das","Shahane","Dharmik","Paji","Shanay"],
-#    'SCORE':[9,8,7,9,9]
-#}
-
-#df = pd.DataFrame(data=data_set)
-#df.set_index('SAP', inplace=True)
-#print(df.head())
 
 df = pd.read_csv('currency.csv')
-#df_head = df.head()
-#df.set_index('Historical Date', inplace=True)
-#print(df['Indian Rupee'])
-#df1 = df[['Historical Date','Indian Rupee']]
-#newdf = df[df.columns[2:3]]
+
 inr=df[df.columns[3]]
 date=df[df.columns[1]]
 data_set={'date':date,'INR':inr}
@@ -28,8 +15,7 @@ final_dataframe.to_csv('currency_v2.csv', header=True)
 #print(file)
 filename = "currency_v2.csv"
   
-# initializing the titles and rows list 
-#fields = [] 
+# initializing and rows list 
 rows = [] 
   
 # reading csv file 
@@ -37,9 +23,6 @@ with open(filename, 'r') as csvfile:
     # creating a csv reader object 
     csvreader = csv.reader(csvfile) 
       
-    # extracting field names through first row 
-    #fields = next(csvreader) 
-  
     # extracting each data row one by one 
     for row in csvreader: 
         rows.append(row) 
@@ -47,31 +30,20 @@ with open(filename, 'r') as csvfile:
     # get total number of rows 
     #print("Total no. of rows: %d"%(csvreader.line_num)) 
   
-# printing the field names 
-#print('Field names are:' + ', '.join(field for field in fields)) 
 xaxis=[]
 yaxis=[]  
 
 #replace 10 with any number acc to your convenience 
 for row in rows[1:10]: 
     # parsing each column of a row 
-    #for col in row: 
-    #    print(col)
-        #print("%10s"%col), 
-    #print('\n')
     xaxis.append(row[0]) 
     yaxis.append(row[1][:6])
 
 #print(xaxis)
 #print(yaxis)
 plt.title('1 USD vs INR')
-
-
 plt.plot(xaxis, yaxis)
-
 plt.xlabel('Day-wise analysis')
-
 plt.ylabel('1 USD in Indian Rupees')
-
 plt.legend()
 plt.show()
